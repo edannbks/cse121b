9ee13afbb6ee7c55e1ef7dcdeffe4b862cc6eb92
edannbks <mor20053@byui.edu>
2023-11-19 17:10:14 -0800

    troubleshooting wk02

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,26 +3,29 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = 'Edith A. Enderle';
-let currentYear = Year();
-let profilePicture = 'images\cse121me.jpg';
+    let fullName = 'Edith A. Enderle';
+    let currentYear = Year();
+    let profilePicture = 'images/cse121me.png';
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById('name');
-const foodElement = document.getElementById('food');
-const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('picture.img');
+    const nameElement = document.getElementById('name');
+    const foodElement = document.getElementById('food');
+    const yearElement = document.querySelector('#year');
+    const imageElement = document.querySelector('picture.img');
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = `<strong>${fullName}</strong>`;
-yearElement.innerText = currentYear;
-imageElement.setAttribute = ('src', profilePicture);
-imageElement.setAttribute = ('alt', 'Profile image of Edith A. Enerle');
+    nameElement.innerHTML = `<strong>${fullName}</strong>`;
+    yearElement.innerText = currentYear;
+    imageElement.setAttribute = ('src', profilePicture);
+    imageElement.setAttribute = ('alt', `Profile image of <strong>${fullName}</strong>`);
 
 /* Step 5 - Array */
 
+    let myFavorites = ['curry','steak','charcuterie','quesabirria','potstickers'];
+    let candy = 'Good and Plenty';
 
-
-
-
-
+    myFavorites.push(candy);
+    foodElement.innerHTML += `<br>${myFavorites}`;
+    myFavorites.shift();
+    foodElement.innerHTML += `<br>${myFavorites}`;
+    
--- w01-task.js
+++ w01-task.js
@@ -14,7 +14,8 @@ let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The total is ${theTotal}`);
 
 // 'total' function declaration
-function total(...theNumbers) {
+function total(...theNumbers)
+{
 	let sum = 0;
 	for (let aNumber of theNumbers) {
 		sum += aNumber * 1; // Why do we use * 1? It implicitly converts a string to a number.
