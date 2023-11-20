edb284d4ba4b35fd90c6224f7537b106a996c729
edannbks <mor20053@byui.edu>
2023-11-19 21:29:55 -0800

    troubleshooting

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,28 +4,28 @@
 
 /* Step 2 - Variables */
     let fullName = 'Edith A. Enderle';
-    let currentYear = Year();
+    let currentYear = '2023';
     let profilePicture = 'images/cse121me.png';
 
 /* Step 3 - Element Variables */
     const nameElement = document.getElementById('name');
     const foodElement = document.getElementById('food');
     const yearElement = document.querySelector('#year');
-    const imageElement = document.querySelector('picture.img');
+    const imageElement = document.querySelector('picture');
 
 /* Step 4 - Adding Content */
     nameElement.innerHTML = `<strong>${fullName}</strong>`;
     yearElement.innerText = currentYear;
-    imageElement.setAttribute = ('src', profilePicture);
+    imageElement.setAttribute = ('src', `<strong>${profilePicture}</strong>`);
     imageElement.setAttribute = ('alt', `Profile image of <strong>${fullName}</strong>`);
 
 /* Step 5 - Array */
 
-    let myFavorites = ['curry','steak','charcuterie','quesabirria','potstickers'];
+    let myFavorites = ['Curry ','Steak ','Charcuterie ','Quesabirria ','Potstickers '];
     let candy = 'Good and Plenty';
 
     myFavorites.push(candy);
     foodElement.innerHTML += `<br>${myFavorites}`;
     myFavorites.shift();
     foodElement.innerHTML += `<br>${myFavorites}`;
-    
+
